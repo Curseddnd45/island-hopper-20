f15d54872383fee13ea853d7c16bf741acb85e1e
Patrick Cole <patrickbgcole@icloud.com>
2025-04-12 08:37:12 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,48 +3,48 @@ tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . 9 9 9 9 9 . . . . . . 
+    . . . . 9 9 8 9 8 9 . . . . . . 
+    . . . . 9 9 9 9 9 9 . . . . . . 
+    . . . . 9 9 9 9 9 9 . . . . . . 
+    . . . . 9 9 8 9 8 9 . . . . . . 
+    9 9 9 9 9 9 9 8 9 9 9 9 9 9 . . 
+    9 9 9 9 9 9 b b b 9 9 9 9 9 . . 
+    9 9 9 9 9 9 9 b 9 9 9 9 9 9 . . 
+    . . 9 9 9 9 b b b 9 9 . . . . . 
+    . . . 9 9 9 9 9 9 9 9 . . . . . 
+    . . 9 9 9 9 . . 9 9 9 9 . . . . 
+    . . 9 9 9 9 . . 9 9 9 9 . . . . 
+    . . 9 9 9 . . . 9 9 9 9 . . . . 
+    . . 9 9 9 . . . . 9 9 9 . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
 forever(function () {
-    Glitch1 = tiles.getTilesByType(assets.tile`myTile1`)._pickRandom()
+    Glitch1 = tiles.getTilesByType(assets.tile`myTile0`)._pickRandom()
     pause(randint(1, 20) * 100)
     tiles.setTileAt(Glitch1, assets.tile`myTile2`)
     pause(randint(1, 20) * 100)
-    tiles.setTileAt(Glitch1, assets.tile`myTile1`)
+    tiles.setTileAt(Glitch1, assets.tile`myTile0`)
 })
 forever(function () {
-    Glitch1 = tiles.getTilesByType(assets.tile`myTile1`)._pickRandom()
+    Glitch1 = tiles.getTilesByType(assets.tile`myTile0`)._pickRandom()
     pause((randint(1, 20) - 1) * 100)
     tiles.setTileAt(Glitch1, assets.tile`myTile2`)
     pause((randint(1, 20) - 1) * 100)
-    tiles.setTileAt(Glitch1, assets.tile`myTile1`)
+    tiles.setTileAt(Glitch1, assets.tile`myTile0`)
 })
 forever(function () {
-    Glitch1 = tiles.getTilesByType(assets.tile`myTile1`)._pickRandom()
-    pause((randint(1, 20) - 2) * 100)
-    tiles.setTileAt(Glitch1, assets.tile`myTile2`)
-    pause((randint(1, 20) - 2) * 100)
-    tiles.setTileAt(Glitch1, assets.tile`myTile1`)
-})
-forever(function () {
-    Glitch1 = tiles.getTilesByType(assets.tile`myTile1`)._pickRandom()
+    Glitch1 = tiles.getTilesByType(assets.tile`myTile0`)._pickRandom()
     pause((randint(1, 20) - 3) * 100)
     tiles.setTileAt(Glitch1, assets.tile`myTile2`)
     pause((randint(1, 20) - 3) * 100)
-    tiles.setTileAt(Glitch1, assets.tile`myTile1`)
+    tiles.setTileAt(Glitch1, assets.tile`myTile0`)
+})
+forever(function () {
+    Glitch1 = tiles.getTilesByType(assets.tile`myTile0`)._pickRandom()
+    pause((randint(1, 20) - 2) * 100)
+    tiles.setTileAt(Glitch1, assets.tile`myTile2`)
+    pause((randint(1, 20) - 2) * 100)
+    tiles.setTileAt(Glitch1, assets.tile`myTile0`)
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,31 +3,52 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "level0":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010101010101010101010000000000010101010101010101010100000000000101010101010101010101000000000000000000010101010101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050808080808080806000000000000000407070707070707070000000000000001020202020202020300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.skillmap.islandTile6,sprites.skillmap.islandTile7,sprites.skillmap.islandTile8,sprites.skillmap.islandTile3,sprites.skillmap.islandTile0,sprites.skillmap.islandTile2,sprites.vehicle.roadHorizontal,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050909090909090806000000000000000407070707070707070000000000000001020202020202020300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.skillmap.islandTile6,sprites.skillmap.islandTile7,sprites.skillmap.islandTile8,sprites.skillmap.islandTile3,sprites.skillmap.islandTile0,sprites.skillmap.islandTile2,sprites.vehicle.roadHorizontal,myTiles.tile3,myTiles.tile1], TileScale.Sixteen);
         }
         return null;
     })
@@ -35,10 +56,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile1":
-            case "tile3":return tile3;
             case "myTile2":
             case "tile4":return tile4;
+            case "myTile0":
+            case "tile1":return tile1;
+            case "Door1":
+            case "tile3":return tile3;
         }
         return null;
     })
